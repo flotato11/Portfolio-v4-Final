932d5569e516671b2fcf6a76d6cc0b24f08f14cd
flotato11 <36093026+flotato11@users.noreply.github.com>
2019-05-08 09:13:04 -0700

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,24 @@
+function menuToggle() {
+  document.getElementsByClassName("menu-button-bar-container")[0].classList.toggle("menu-button-bar-container--opened");
+  document.getElementsByClassName("fs-menu")[0].classList.toggle("fs-menu--opened");
+}
+
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}

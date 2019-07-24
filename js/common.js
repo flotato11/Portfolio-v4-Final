3e68fbc824351870342c0dea74e64216c322076e
flotato11 <36093026+flotato11@users.noreply.github.com>
2019-07-23 22:03:56 -0700

    ColorSwitch Added

--- js/common.js
+++ js/common.js
@@ -3,6 +3,26 @@ function menuToggle() {
   document.getElementsByClassName("fs-menu")[0].classList.toggle("fs-menu--opened");
 }
 
+$(document).ready(function() {
+
+  var c = 1;
+
+  $('#colorswap-icon').click(function() {
+    c++;
+    console.log(c);
+    if (c % 2 === 0) {
+      $(':root').css('--mainbackground', 'rgba(209,243,255,1)');
+      $(':root').css('--floodlight', 'rgba(255,209,220,.8)');
+      $(':root').css('--color1', 'black');
+    }
+    else {
+      $(':root').css('--mainbackground', '#1a1a1a')
+      $(':root').css('--floodlight', '#e0d692');
+      $(':root').css('--color1', 'white');
+    }
+  });
+});
+
 var slideIndex = [1,1];
 var slideId = ["mySlides1", "mySlides2"]
 showSlides(1, 0);
